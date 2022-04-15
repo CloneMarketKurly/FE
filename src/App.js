f0380e04148a34e8d38bd183b53dd90e865585c8
JIEUNNN <pandaa0628@gmail.com>
2022-04-15 19:13:23 +0900

    route 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
+
+import Main from './pages/Main'
+import Detail from './pages/Detail'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import ReviewWrite from './pages/ReviewWrite'
 
 function App() {
   return (
     <React.Fragment>
-      메인
+      <BrowserRouter>
+        <Route path="/" exact component={Main}/>
+        <Route path="/login" exact component={Login}/>
+        <Route path="/signup" exact component={Signup}/>
+        <Route path="/detail" exact component={Detail}/>
+        <Route path="/reviewWrite" exact component={ReviewWrite}/>
+      </BrowserRouter>
     </React.Fragment>
   );
 }

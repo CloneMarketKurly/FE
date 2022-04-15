1ae7bed0ac4eb219f266d3e846cdad4c93903784
JIEUNNN <pandaa0628@gmail.com>
2022-04-16 03:52:18 +0900

    footer 구현완료

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Card = () => {
   return (
-    <React.Fragment>Card</React.Fragment>
+    <React.Fragment>
+      <DivSt>
+        {/* <img src='https://i.esdrop.com/d/f/zoDvw3Gypq/PzbrOv6Lin.png'/> */}
+        {/* <img src='/img/insta.png'/> */}
+
+      </DivSt>
+    </React.Fragment>
   )
 }
 
+const DivSt = styled.div`
+  width: 1000px;
+  margin: auto;
+
+`
 export default Card;
